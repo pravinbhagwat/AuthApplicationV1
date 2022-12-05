625d9805fb383957fcc146c006335e352af907f7
Pravin Bhagwat <pravinbhagwat150@gmail.com>
2022-12-05 11:18:26 +0530

    Camera & Gallery Task Completed

--- app/src/main/java/com/example/authapplicationv1/ImageviewActivity.java
+++ app/src/main/java/com/example/authapplicationv1/ImageviewActivity.java
@@ -1,17 +1,21 @@
 package com.example.authapplicationv1;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.imageview.ShapeableImageView;
@@ -22,8 +26,8 @@ public class ImageviewActivity extends AppCompatActivity {
     MaterialButton clickImage, selectImage;
     MaterialButton exit;
 
-    private final int CAMERA_REQ_CODE = 100;
     ActivityResultLauncher<String> mTakePhoto;
+    ActivityResultLauncher<Intent> CTakePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +39,34 @@ public class ImageviewActivity extends AppCompatActivity {
         selectImage = findViewById(R.id.button_selactImage);
         exit  = findViewById(R.id.button_exit);
 
-            mTakePhoto = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
-                @Override
-                public void onActivityResult(Uri result) {
-                    imageView.setImageURI(result);
+
+        // Selecting Image from Gallery
+        mTakePhoto = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                imageView.setImageURI(result);
+            }
+        });
+
+        // Capturing Image from Camera
+        CTakePhoto = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if(result.getResultCode() == Activity.RESULT_OK) {
+                    Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
+                    imageView.setImageBitmap(bitmap);
                 }
-            });
+                else {
+                    Toast.makeText(getApplicationContext(), "Camera is not working", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         clickImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                //Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                CTakePhoto.launch(intent);
             }
         });
 
